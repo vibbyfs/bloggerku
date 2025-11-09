570af1a23321d61e17ac9b47ecafc988aaf90029
vibbyfs <vibbyfs@gmail.com>
2025-11-09 21:30:53 +0700

    feat: restore config.js with database configuration for development, test, and production environments

---
+++ server/config/config.js
@@ -0,0 +1,32 @@
+require("dotenv").config();
+
+module.exports = {
+  development: {
+    username: "postgres",
+    password: "postgres",
+    database: "BLOGGERKU",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    logging: false
+  },
+  test: {
+    username: "postgres",
+    password: "postgres",
+    database: "BLOGGERKU_TEST",
+    host: "127.0.0.1",
+    dialect: "postgres",
+    logging: false
+  },
+  production: {
+    use_env_variable: "DATABASE_URL",
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      },
+      family: 4
+    },
+    logging: false
+  }
+};
