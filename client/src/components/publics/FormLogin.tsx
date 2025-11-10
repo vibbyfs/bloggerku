7a8ff1c16cb9452c7a680ee09343be688bb3958d
vibbyfs <vibbyfs@gmail.com>
2025-11-10 15:37:17 +0700

    feat: simplify LoginPage layout and enhance FormLogin structure for improved readability

--- client/src/components/publics/FormLogin.tsx
+++ client/src/components/publics/FormLogin.tsx
@@ -25,47 +25,53 @@ export default function FormLogin() {
 
   return (
     <>
-      <form onSubmit={handleSubmit} className="space-y-4">
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            Email
-          </label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-            className="form-input w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-gray-200"
-            placeholder="you@example.com"
-          />
-        </div>
+      <div className="flex items-center justify-center min-h-screen bg-gray-50 px-4">
+        <div className="w-full max-w-md bg-white border border-gray-200 shadow-md rounded-xl p-8">
+          <form onSubmit={handleSubmit} className="space-y-4">
+            <h1 className="text-2xl font-bold text-center mb-8">
+              Login to Your Account
+            </h1>
 
-        <div>
-          <label className="block text-sm font-medium text-gray-700 mb-1">
-            Password
-          </label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            className="form-input w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-200 focus:border-gray- mb-5"
-            placeholder="••••••••"
-          />
-        </div>
-        <div>
-          <Button label="Login" type="submit" className="w-full" />
-        </div>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Email
+              </label>
+              <input
+                type="email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                placeholder="you@example.com"
+              />
+            </div>
 
-        <div className="flex justify-center mt-5">
-          <p className="text-gray-500 hover:text-gray-600 pt-5 text-sm">
-            No Account yet?{" "}
-            <Link href="/">
-              <span className="hover:text-blue-400">
-                Feel free to explore our blog section.
-              </span>
-            </Link>
-          </p>
+            <div>
+              <label className="block text-sm font-medium text-gray-700 mb-1">
+                Password
+              </label>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                className="w-full border border-gray-300 rounded px-4 py-2 focus:outline-none focus:ring-2 focus:ring-gray-200"
+                placeholder="••••••••"
+              />
+            </div>
+
+            <Button label="Login" type="submit" className="w-full" />
+
+            <div className="text-center mt-5">
+              <p className="text-gray-500 text-sm">
+                <Link href="/">
+                  <span className="hover:text-blue-400">
+                    Feel free to explore our blog section.
+                  </span>
+                </Link>
+              </p>
+            </div>
+          </form>
         </div>
-      </form>
+      </div>
     </>
   );
 }
