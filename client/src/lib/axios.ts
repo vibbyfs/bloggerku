30a7b2b638c6034d0060588fe60e8c6bdb9f809d
vibbyfs <vibbyfs@gmail.com>
2025-12-22 15:25:36 +0700

    feat: update axios client to use dynamic API_URL and adjust server port mapping in docker-compose

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
+
 export const axiosClient = axios.create({
-  baseURL: "http://localhost:3000",
+  baseURL: API_URL,
 });
