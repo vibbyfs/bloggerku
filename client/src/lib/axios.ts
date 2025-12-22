120031cb60a6641e2341989bd8d853cbe0e2d9fa
vibbyfs <vibbyfs@gmail.com>
2025-12-22 15:20:38 +0700

    fix: update production database host to localhost in config.json

--- client/src/lib/axios.ts
+++ client/src/lib/axios.ts
@@ -1,7 +1,5 @@
 import axios from "axios";
 
-const API_URL = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
-
 export const axiosClient = axios.create({
-  baseURL: API_URL,
+  baseURL: "http://localhost:3000",
 });
